6738fc4bab193d71ad59293cb55e1ab18bc54644
Wang <yimei.wym@gmail.com>
2019-04-19 11:32:38 +0800

    First commit

---
+++ deaf_aunty.py
@@ -0,0 +1,23 @@
+# Keeps looping until magic words are spoken
+# Gets input from user
+
+
+def speak_to_auntie():
+    while True:
+        user_input = input("Speak to aunty: \n")
+
+        if user_input == "I love you aunty, I have to go now":
+            print("ok. Goodbye\n")
+            return False
+
+        if user_input.islower():
+            print("WHAT? SPEAK UP! \n")
+
+        elif user_input.isupper():
+            print("YOU ARE VERY RUDE! \n")
+
+        else:
+            print("SHOW SOME RESPECT! \n")
+
+
+speak_to_auntie()
